041a9a3088f9c259b7a98888d1f3b9ed61ba3bcd
Ирина Черняева <iridache@Irinas-MacBook-Air.local>
2025-11-23 11:45:47 +0300

    chore(''): add tasks

--- src/components/FlightList.tsx
+++ src/components/FlightList.tsx
@@ -1,3 +1,3 @@
-import flights from '../mock-data/flights.json';
+// import flights from '../mock-data/flights.json';
 
 // написать реакт компонент, который проходится по массиву и возвращает div с полями iata и  airline name
