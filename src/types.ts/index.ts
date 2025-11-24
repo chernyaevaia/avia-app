ef7ffd72ac69a08b565d39223088e9576823eb2e
Вячеслав Ефимов <trooper.wers@yandex.ru>
2025-11-24 13:45:03 +0300

    chore: add flight data interfaces

--- src/types.ts/index.ts
+++ src/types.ts/index.ts
@@ -1,1 +1,88 @@
-// описать тип flights
+export interface AviationStackResponse {
+  pagination: Pagination;
+  data: FlightData[];
+}
+
+export interface Pagination {
+  limit: number;
+  offset: number;
+  count: number;
+  total: number;
+}
+
+export interface FlightData {
+  flight_date: string;
+  flight_status: FlightStatus;
+  departure: FlightEndpoint;
+  arrival: FlightEndpoint
+  airline: AirlineInfo;
+  flight: FlightInfo;
+  aircraft: AircraftInfo | null;
+  live: LiveInfo | null;
+}
+
+export interface FlightEndpoint {
+  airport: string;
+  timezone: string;
+  iata: string;
+  icao: string;
+  terminal: string | null;
+  gate: string | null;
+  delay: number;
+  scheduled: string;
+  estimated: string | null;
+  actual: string | null;
+  estimated_runway: string | null;
+  actual_runway: string | null;
+  baggage: string | null;
+}
+
+export interface AirlineInfo {
+  id: string;
+  fleet_average_age: number | null;
+  airline_id: string | null;
+  callsign: string;
+  hub_code: string;
+  iata_code: string;
+  icao_code: string;
+  country_iso2: string;
+  date_founded: string | null;
+  iata_prefix_accounting: string | null;
+  airline_name: string;
+  country_name: string;
+  fleet_size: number | null;
+  status: string;
+  type: string;
+}
+
+export interface FlightInfo {
+  number: string;
+  iata: string;
+  icao: string;
+  codeshared: Record<string, unknown> | null;
+}
+
+export interface AircraftInfo {
+  registration: string;
+  iata: string;
+  icao: string;
+  icao24: string;
+}
+
+export interface LiveInfo {
+  updated: string;
+  latitude: number;
+  longitude: number;
+  altitude: number;
+  direction: number;
+  speed_horizontal: number;
+  speed_vertical: number;
+  is_ground: boolean;
+}
+export type FlightStatus =
+  | "scheduled"
+  | "active"
+  | "landed"
+  | "cancelled"
+  | "incident"
+  | "diverted";
